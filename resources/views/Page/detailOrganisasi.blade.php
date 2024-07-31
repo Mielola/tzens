c765e3eb9c51c8d08612b0f1973fa5f08870c8a2
Muhammad Wildan Dhiya Ulhaq <mwildab15@gmail.com>
2024-07-31 18:48:50 +0700

    Page Detail Organisasi

---
+++ resources/views/Page/detailOrganisasi.blade.php
@@ -0,0 +1,12 @@
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot:title>
+    <article class="py-8 max-w-screen-md border-b-2">
+        <h2 class="text-3xl hover:underline font-bold tracking-tighter text-gray-900">Judul Artikel 1</h2>
+        <div class="text-base text-gray-500">
+            <a href="">{{ $post['author'] }} | {{ $post['date'] }}</a>
+        </div>
+        <p class="my-4 sm:text-justify lg:text-left">{{ Str::limit($post['content'], 200) }}</p>
+        <a href="/organisasi" class="font-medium text-blue-500">&laquo; Read More;</a>
+    </article>
+
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Arr;
 
 Route::get('/', function () {
     return view('page/welcome', ['title' => 'Home Page']);
@@ -35,6 +36,40 @@ Route::get('/organisasi', function () {
         ]
     ]);
 });
+
+Route::get('/organisasi/{slug}', function ($slug) {
+    $organisasi = [
+        [
+            'id' => 1,
+            'title' => 'Himpunan Rekayasa Perangkat Lunak',
+            'author' => 'Muhammad Wildan',
+            'date' => '31 Juli 2024',
+            'slug' => 'organisasi-1',
+            'content' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti,
+            ullam necessitatibus est atque labore eos minus vero sapiente enim, animi magni, autem in dolorum reiciendis
+            repellat provident distinctio corporis laboriosam.',
+
+        ],
+        [
+            'id' => 2,
+            'title' => 'Himpunan Teknologi Telekomunikasi',
+            'author' => 'Tatang Supratman',
+            'date' => '09 Juli 2024',
+            'slug' => 'organisasi-2',
+            'content' => 'adipisicing elit. Corrupti,
+            ullam necessitatibus est atque labore eos minus vero sapiente enim, animi magni, autem in dolorum reiciendis
+            repellat provident distinctio corporis laboriosam.',
+
+        ],
+    ];
+
+    $organ = Arr::first($organisasi, function ($organ) use ($slug) {
+        return $organ['slug'] == $slug;
+    });
+
+    return view('page/detailOrganisasi', ['title' => 'Detail Organisasi', 'post' => $organ]);
+});
+
 
 Route::get('/webinar', function () {
     return view('page/webinar', ['title' => 'Webinar Page']);
