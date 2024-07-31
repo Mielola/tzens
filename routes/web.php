ac173a451fdba0810b8f968d443fdd6ce05f00f1
Muhammad Wildan Dhiya Ulhaq <mwildab15@gmail.com>
2024-07-31 20:47:33 +0700

    Membuat Model Organisasi

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Models\Organisasi;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Arr;
+
+
+
 
 Route::get('/', function () {
     return view('page/welcome', ['title' => 'Home Page']);
@@ -10,62 +13,13 @@ Route::get('/', function () {
 Route::get('/organisasi', function () {
     return view('page/organisasi', [
         'title' => 'Organisasi Page',
-        'post' => [
-            [
-                'id' => 1,
-                'title' => 'Himpunan Rekayasa Perangkat Lunak',
-                'author' => 'Muhammad Wildan',
-                'date' => '31 Juli 2024',
-                'slug' => 'organisasi-1',
-                'content' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti,
-                ullam necessitatibus est atque labore eos minus vero sapiente enim, animi magni, autem in dolorum reiciendis
-                repellat provident distinctio corporis laboriosam.',
-
-            ],
-            [
-                'id' => 2,
-                'title' => 'Himpunan Teknologi Telekomunikasi',
-                'author' => 'Tatang Supratman',
-                'date' => '09 Juli 2024',
-                'slug' => 'organisasi-2',
-                'content' => 'adipisicing elit. Corrupti,
-                ullam necessitatibus est atque labore eos minus vero sapiente enim, animi magni, autem in dolorum reiciendis
-                repellat provident distinctio corporis laboriosam.',
-
-            ],
-        ]
+        'post' => Organisasi::ViewOrganisasi()
     ]);
 });
 
 Route::get('/organisasi/{slug}', function ($slug) {
-    $organisasi = [
-        [
-            'id' => 1,
-            'title' => 'Himpunan Rekayasa Perangkat Lunak',
-            'author' => 'Muhammad Wildan',
-            'date' => '31 Juli 2024',
-            'slug' => 'organisasi-1',
-            'content' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti,
-            ullam necessitatibus est atque labore eos minus vero sapiente enim, animi magni, autem in dolorum reiciendis
-            repellat provident distinctio corporis laboriosam.',
 
-        ],
-        [
-            'id' => 2,
-            'title' => 'Himpunan Teknologi Telekomunikasi',
-            'author' => 'Tatang Supratman',
-            'date' => '09 Juli 2024',
-            'slug' => 'organisasi-2',
-            'content' => 'adipisicing elit. Corrupti,
-            ullam necessitatibus est atque labore eos minus vero sapiente enim, animi magni, autem in dolorum reiciendis
-            repellat provident distinctio corporis laboriosam.',
-
-        ],
-    ];
-
-    $organ = Arr::first($organisasi, function ($organ) use ($slug) {
-        return $organ['slug'] == $slug;
-    });
+    $organ = Organisasi::find($slug);
 
     return view('page/detailOrganisasi', ['title' => 'Detail Organisasi', 'post' => $organ]);
 });
